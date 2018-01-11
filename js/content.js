b537b0aab8fbd4f9a39b61723b00f7bb96cb40d0
Sam <sfkrieger@gmail.com>
2018-01-11 11:06:41 -0800

    Not obscene. Articles and Projects under construction.

---
+++ js/content.js
@@ -0,0 +1,7 @@
+//load navbar - first element in body...
+//this elem:   <div class="container-fluid navbar-container m-sm-5">
+
+//then in the row element...:
+  // add Title
+  // add content
+//for each
